c1d3e9ab26dfd291d14a45770243f15885f66903
raul3222 <sh_raul08@mail.ru>
2021-12-08 21:08:06 +0200

    Initial Commit

---
+++ CoreDataDemo/TaskListViewController.swift
@@ -0,0 +1,170 @@
+//
+//  TaskListViewController.swift
+//  CoreDataDemo
+//
+//  Created by Raul Shafigin on 06.12.2021.
+//
+
+import UIKit
+
+protocol TaskViewControllerDelegate {
+    func setNewTask(task: Task)
+}
+
+class TaskListViewController: UITableViewController {
+    
+    private let cellID = "task"
+    private let context = StorageManager.shared.persistentContainer.viewContext
+    
+    private var taskList: [Task] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        view.backgroundColor = .white
+        setupNavigationBar()
+        fetchData()
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        fetchData()
+//        tableView.reloadData()
+//    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navBarAppearance.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        showAlert(with: "New task", and: "What do you want to do?")
+    }
+    
+//    @objc private func addNewTask() {
+//        let taskVC = TaskViewController()
+//        taskVC.delegate = self
+//        present(taskVC, animated: true)
+//    }
+    
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        
+        do{
+            taskList = try context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        let task = Task(context: context)
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error)
+            }
+        }
+    }
+    
+    private func deleteTask(at index: Int){
+//        context.delete(taskList[index])
+//        taskList.remove(at: index)
+        
+        
+        let objectToDelete = taskList[index]
+               taskList.remove(at: index)
+               context.delete(objectToDelete)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            deleteTask(at: indexPath.row)
+            //contacts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        } else if editingStyle == .insert {
+            print("got you")
+        }
+    }
+}
+
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension TaskListViewController: TaskViewControllerDelegate {
+    func setNewTask(task: Task) {
+        //fetchData()
+        taskList.append(task)
+        tableView.reloadData()
+    }
+}
+
